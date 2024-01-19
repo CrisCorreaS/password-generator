fe484fb73430b565d872575c3a7e78a0ea052884
Cristina Correa <cristina.correa.segade@gmail.com>
2024-01-19 06:20:55 +0100

    project almost ready

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ const CHECK_MAYUSCULAS = document.querySelector("#uppercase");
 const CHECK_MINUSCULAS = document.querySelector("#lowercase");
 const CHECK_NUMEROS = document.querySelector("#numbers");
 const CHECK_SIMBOLOS = document.querySelector("#symbols");
+
 const PASSWORD_LENGTH = document.querySelector("input[type=range]");
+const PASSWORD_LENGTH_NUMBER = document.querySelector(".char-count");
 
 const PASSWORD = document.querySelector(".password-placeholder");
 const TEXTO_SEGURIDAD = document.querySelector(".strength-rating-text");
+const BARRAS_SEGURIDAD = document.querySelectorAll(".bar");
 
 let mayusculas = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
 let minusculas = "abcdefghijklmnñopqrstuvwxyz";
@@ -41,6 +44,7 @@ function generarContrasinal() {
       for (let j = 0; j < criterios.length; j++) {
         criterioContrasinal += criterios[j];
       }
+      contadorChecks = checks.length;
     } else {
       for (let k = 0; k < criterios.length; k++) {
         if (checks[k]) {
@@ -50,7 +54,7 @@ function generarContrasinal() {
       }
     }
 
-    for (let l = 0; l < PASSWORD_LENGTH.value; l++) {
+    for (let l = 0; l < PASSWORD_LENGTH.valueAsNumber; l++) {
       let numero = Math.floor(Math.random() * criterioContrasinal.length);
 
       contrasinal += criterioContrasinal.charAt(numero);
@@ -62,24 +66,72 @@ function generarContrasinal() {
   return contadorChecks;
 }
 
-function generarCriterioSeguridad(numeroCaracteres, cantidadChecks){
-    let valoresSeguridad = ["VERY WEAK", "WEAK", "MEDIUM", "STRONG"];
+function gestionSeguridad(numero) {
+  let valoresSeguridad = ["VERY-WEAK", "WEAK", "MEDIUM", "STRONG"];
 
-    switch(cantidadChecks){
-        case 0: 
-
-
+  BARRAS_SEGURIDAD.forEach((barra) => {
+    for (let m = 0; m < valoresSeguridad.length; m++) {
+      barra.classList.remove(valoresSeguridad[m].toLowerCase());
     }
+  });
+
+  for (let n = 0; n <= numero; n++) {
+    BARRAS_SEGURIDAD[n].classList.add(valoresSeguridad[numero].toLowerCase());
+  }
+  TEXTO_SEGURIDAD.innerHTML = valoresSeguridad[numero].replace("-", " ");
 }
 
+function generarCriterioSeguridad(numeroCaracteres, cantidadChecks) {
+  if (numeroCaracteres < 12) {
+    gestionSeguridad(0);
+  } else {
+    switch (cantidadChecks) {
+      case 0:
+        gestionSeguridad(cantidadChecks); // 0
+        break;
+      case 1:
+        gestionSeguridad(cantidadChecks - 1); // 0
+        break;
+      case 2:
+        gestionSeguridad(cantidadChecks - 1); // 1
+        break;
+      case 3:
+        numeroCaracteres > 15
+          ? gestionSeguridad(cantidadChecks - 1) // 2
+          : gestionSeguridad(cantidadChecks - 2); // 1
+        break;
+      case 4:
+        numeroCaracteres > 17
+          ? gestionSeguridad(cantidadChecks - 1) // 3
+          : gestionSeguridad(cantidadChecks - 2); // 2
+        break;
+    }
+  }
+}
 
-function gestionContrasinal(e){
-    let cantidadChecks = generarContrasinal();
+function gestionContrasinal(e) {
+  e.preventDefault();
+  let cantidadChecks = generarContrasinal();
 
-    generarCriterioSeguridad(PASSWORD_LENGTH.value, cantidadChecks);
-
-    e.preventDefault();
+  generarCriterioSeguridad(PASSWORD_LENGTH.valueAsNumber, cantidadChecks);
 }
 
 BOTON_SUBMIT.addEventListener("click", gestionContrasinal, false);
-//BOTON_COPIAR.addEventListener("click", copiarContrasinal, false);
+
+function copiarContrasinal() {
+  if (PASSWORD.innerHTML != "P4$5W0rD!") {
+    navigator.clipboard.writeText(PASSWORD.innerHTML);
+  }
+}
+
+BOTON_COPIAR.addEventListener("click", copiarContrasinal, false);
+
+PASSWORD_LENGTH.addEventListener("input", () => {
+    PASSWORD_LENGTH_NUMBER.innerHTML = PASSWORD_LENGTH.valueAsNumber;
+
+    let porcentaje = (PASSWORD_LENGTH.valueAsNumber - PASSWORD_LENGTH.min) /(PASSWORD_LENGTH.max - PASSWORD_LENGTH.min);
+    PASSWORD_LENGTH.style.setProperty("--seek-before-width", `${porcentaje * 100}%`); // Así se cambia el valor de una variable de css
+  },
+  false
+);
+// The "oninput event" occurs immediately after the content or value of an <input> or <textarea> element has been changed, while "onchange" occurs when the element loses focus and the onchange event also works on <select> elements
